429c48993b4bc11d91ade3697170a56a4627ae1d
jectluci <esteafnoject@gmail.com>
2025-07-17 09:10:58 -0500

    Y que sale??

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -9,12 +9,15 @@ return {
 {
     '2kabhishek/nerdy.nvim',
     dependencies = {
-        'stevearc/dressing.nvim',
-        'nvim-telescope/telescope.nvim',
+        'folke/snacks.nvim',
     },
     cmd = 'Nerdy',
+    opts = {
+        max_recents = 30, -- Configure recent icons limit
+        add_default_keybindings = true, -- Add default keybindings
+        use_new_command = true, -- Enable new command system
+    }
 },
-
 --Status bar
 { 'nvim-lualine/lualine.nvim', 
   event = {'BufReadPre', 'BufNewFile'},
